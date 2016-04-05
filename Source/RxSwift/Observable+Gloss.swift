8d064fb6330aaec1e7684789467cb1df56a9f30a
steven rogers <spxrogers@gmail.com>
2016-04-05 17:25:24 -0500

    Added: RxSwift Observable support.

--- Source/RxSwift/Observable+Gloss.swift
+++ Source/RxSwift/Observable+Gloss.swift
@@ -12,5 +12,21 @@ import Gloss
 
 /// Extension for transforming Responses into Decodable object via Gloss
 public extension ObservableType where E == Response {
-  // todo
+
+  /// Maps response data into an Observable of a type that implements the Decodable protocol.
+  /// Observable .Errors's on failure.
+  public func mapObject<T: Decodable>(type: T.Type) -> Observable<T> {
+    return flatMap { response -> Observable<T> in
+      return Observable.just(try response.mapObject())
+    }
+  }
+
+  /// Maps response data into an Observable of an array of a type that implements the Decodable protocol.
+  /// Observable .Errors's on failure.
+  public func mapArray<T: Decodable>(type: T.Type) -> Observable<[T]> {
+    return flatMap { (response) -> Observable<[T]> in
+      return Observable.just(try response.mapArray())
+    }
+  }
+
 }
